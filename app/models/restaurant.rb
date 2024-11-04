b24f3a372cd46de2e779ce6f4d2dd8f29229bd3e
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2024-11-04 10:41:09 -0300

    Restaurant model configured

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :def destroy
+
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, presence: true #validates :name, :adress, presence: :true (Posso colocar tudo junto em uma linha só)
+  validates :address, presence: true
+  validates :category, presence: true, inclusion { in: CATEGORIES }
 end
