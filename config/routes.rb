3e68a2bfccfb634b3882d02991d0bb665db18fd0
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2024-11-05 17:14:02 -0300

    routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "restaurants#index"
+
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show'
+  post 'restaurants', to: 'restaurants#create'
+  # ou resources :restaurants, only: [:index, :new, :show, :create]
+
+  # resources :restaurants, only: [:index, :new, :show, :create] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+  resources :restaurants do
+    resources :reviews, only: %i[new create] # ou ==> [:new, :create]
+  end
 end
