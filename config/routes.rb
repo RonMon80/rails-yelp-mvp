f9447a4a09157e5014b9bd2075d1681971113965
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2024-11-06 18:22:41 -0300

    single quotes routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "restaurants#index"
+  root 'restaurants#index'
 
   # get 'restaurants', to: 'restaurants#index'
   # get 'restaurants/new', to: 'restaurants#new'
