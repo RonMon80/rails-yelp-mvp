73bd6f68e8a9fc72532bfe8135cb793124661ab8
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2024-11-06 14:39:55 -0300

    review controller and routes created

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,18 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "restaurants#index"
 
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new'
-  get 'restaurants/:id', to: 'restaurants#show'
-  post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # post 'restaurants', to: 'restaurants#create'
   # ou resources :restaurants, only: [:index, :new, :show, :create]
+
+
 
   # resources :restaurants, only: [:index, :new, :show, :create] do
   #   resources :reviews, only: [:new, :create]
   # end
-  resources :restaurants do
+  resources :restaurants, only: %i[index new show create] do # ou ==> [:index, :new, :show, :crete]
     resources :reviews, only: %i[new create] # ou ==> [:new, :create]
   end
 end
